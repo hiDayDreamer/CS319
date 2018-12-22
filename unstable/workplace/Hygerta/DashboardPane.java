b159a808e2832f0586673a13ccd82ad453d45d02
Hygerta Imeri <29503555+hiDayDreamer@users.noreply.github.com>
2018-12-22 15:43:44 +0300

    Add files via upload

--- unstable/workplace/Hygerta/DashboardPane.java
+++ unstable/workplace/Hygerta/DashboardPane.java
@@ -16,10 +16,14 @@ import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.ProgressIndicator;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.StrokeLineCap;
 
 public class DashboardPane extends Pane {
     //Constants
     private final String BACK_ICON = "/img/backIcon.png";
+    private final String SOUND_ICON = "/img/soundIcon.png";
+    private final String SETTINGS_ICON = "/img/Settings-icon.png";
     private final String COPYRIGHT_LABEL = "Developed by Royal Flush";
     private final double WIDTH  = 1080;
     private final double HEIGHT = 720;
@@ -43,6 +47,8 @@ public class DashboardPane extends Pane {
     //Buttons
     private Button backButton;
     private Button[] pagePassButtons;
+    private Button soundButton;
+    private Button settingsButton;
 
     //Images
     private Image backImage;
@@ -85,11 +91,35 @@ public class DashboardPane extends Pane {
         backButton.setLayoutX(25);
         backButton.setLayoutY(35);
 
-        Label dashboard = new Label("Dashboard");
+
+        ImageView soundImage = new ImageView(new Image(SOUND_ICON));
+        soundImage.setFitWidth(ICON_SIZE);
+        soundImage.setFitHeight(ICON_SIZE);
+        soundButton = new Button();
+        soundButton.setStyle("-fx-background-color: transparent");
+        soundButton.setMinSize(ICON_SIZE,ICON_SIZE);
+        soundButton.setLayoutX(WIDTH - 100);
+        soundButton.setLayoutY(25);
+
+        soundButton.setGraphic(soundImage);
+
+        settingsButton = new Button();
+        settingsButton.setStyle("-fx-background-color: transparent");
+        settingsButton.setMinSize(ICON_SIZE, ICON_SIZE);
+        settingsButton.setLayoutX(WIDTH - 200);
+        settingsButton.setLayoutY(25);
+        settingsButton.setGraphic(new ImageView(SETTINGS_ICON));
+
+        Label dashboard = new Label("");
+        ImageView labelImage = new ImageView("/img/dashboard_label.png");
+        labelImage.setFitWidth(400);
+        labelImage.setFitHeight(60);
+
+        dashboard.setGraphic(labelImage);
         dashboard.setStyle("-fx-font-weight: bold;");
         dashboard.setMaxSize(450,75);
         dashboard.setFont(new Font("Verdana", 45));
-        dashboard.setLayoutX(400);
+        dashboard.setLayoutX(325);
         dashboard.setLayoutY(50);
 
 
@@ -102,8 +132,8 @@ public class DashboardPane extends Pane {
         //chart.setTitle("Win/Lose");
         chart.setLegendVisible(false);
         chart.setLayoutX(50);
-        chart.setLayoutY(325);
-        chart.setMaxSize(350,350);
+        chart.setLayoutY(350);
+        chart.setMaxSize(320,320);
 
         final Label caption = new Label("");
         caption.setStyle("-fx-font-weight: bold;");
@@ -133,13 +163,13 @@ public class DashboardPane extends Pane {
         barChart.getData().addAll(series1, series2, series3);
         barChart.setBarGap(-15);
         for (Node n : barChart.lookupAll(".default-color0.chart-bar")) {
-            n.setStyle("-fx-bar-fill: blue;");
+            n.setStyle("-fx-bar-fill: #0a3957;");
         }
         for (Node n : barChart.lookupAll(".default-color1.chart-bar")) {
-            n.setStyle("-fx-bar-fill: #ff1f79;");
+            n.setStyle("-fx-bar-fill: #65092b;");
         }
         for (Node n : barChart.lookupAll(".default-color2.chart-bar")) {
-            n.setStyle("-fx-bar-fill: #88ffd4;");
+            n.setStyle("-fx-bar-fill: #366655;");
         }
 
         for (Node n : barChart.lookupAll(".chart-plot-background")) {
@@ -154,7 +184,7 @@ public class DashboardPane extends Pane {
             n.setStyle("-fx-background-color: transparent");
         }
 
-        barChart.setLayoutX(50);
+        barChart.setLayoutX(75);
         barChart.setLayoutY(115);
         barChart.setMaxSize(250,250);
         barChart.setLegendVisible(false);
@@ -163,9 +193,9 @@ public class DashboardPane extends Pane {
         ProgressIndicator gameStatusIndicator = new ProgressIndicator();
         gameStatusIndicator.setProgress(gameStatus);
         gameStatusIndicator.setMinSize(200,200);
-        gameStatusIndicator.setStyle("-fx-progress-color: #ff2c47;");
-        gameStatusIndicator.setLayoutX(725);
-        gameStatusIndicator.setLayoutY(85);
+        gameStatusIndicator.setStyle("-fx-progress-color: rgba(255,51,56,0.81);");
+        gameStatusIndicator.setLayoutX(750);
+        gameStatusIndicator.setLayoutY(130);
 
         StackPane starsCollectedStack = new StackPane();
         ImageView starsCollected = new ImageView("/img/star-icon.png");
@@ -179,42 +209,73 @@ public class DashboardPane extends Pane {
         starsCollectedStack.getChildren().addAll(starsCollected, stars);
 
         starsCollectedStack.setLayoutX(750);
-        starsCollectedStack.setLayoutY(350);
+        starsCollectedStack.setLayoutY(400);
 
 
         Pane skinsPane = new Pane();
         pagePassButtons = new Button[2];
         pagePassImages = new Image[2];
-        pagePassImages[1] = new Image(getClass().getResourceAsStream("/img/play-symbol.png"), 16, 48, false, false);
-        pagePassImages[0] = new Image(getClass().getResourceAsStream("/img/left-angle-bracket.png"), 16, 48, false, false);
+        pagePassImages[1] = new Image(getClass().getResourceAsStream("/img/next.png"), 30, 48, true, false);
+        pagePassImages[0] = new Image(getClass().getResourceAsStream("/img/prev.png"), 30, 48, true, false);
         for ( int i = 0; i < 2; i++){
             pagePassButtons[i] = new Button();
             pagePassButtons[i].setMaxSize(16, 48);
             pagePassButtons[i].setGraphic(new ImageView(pagePassImages[i]));
-            //backButton[i].setStyle("-fx-background-color: transparent");
+            pagePassButtons[i].setStyle("-fx-background-color: transparent");
         }
-        pagePassButtons[0].setLayoutX(0);
+        pagePassButtons[0].setLayoutX(-10);
         pagePassButtons[0].setLayoutY(75);
-        pagePassButtons[1].setLayoutX(123);
+        pagePassButtons[1].setLayoutX(125);
         pagePassButtons[1].setLayoutY(75);
 
-        skinsPane.setStyle("-fx-border-color: black");
+        //skinsPane.setStyle("-fx-border-color: black");
         ImageView skin1 = new ImageView("/img/5-1.png");
         skin1.setFitWidth(100);
         skin1.setFitHeight(175);
         skin1.setLayoutX(28);
-        skinsPane.setLayoutX(515);
+        skinsPane.setLayoutX(450);
         skinsPane.setLayoutY(425);
         skinsPane.getChildren().addAll(pagePassButtons[0], skin1, pagePassButtons[1]);
+        Line line1 = new Line(450, 375, 325, 425);
 
         ImageView profile = new ImageView("/img/userInfoIcon.png");
         profile.setFitWidth(300);
         profile.setFitHeight(200);
-        profile.setLayoutX(360);
+        profile.setLayoutX(375);
         profile.setLayoutY(175);
-        Line line = new Line(450, 375, 325, 425);
 
-        this.getChildren().addAll(dashboard,backButton, chart, caption, barChart, gameStatusIndicator, starsCollectedStack, skinsPane, profile, line);
+        Line lineToBarChart = new Line(435, 240, 325, 200);
+        lineToBarChart.setStroke(Color.BLACK);
+        lineToBarChart.setStrokeWidth(2);
+        lineToBarChart.setStrokeLineCap(StrokeLineCap.BUTT);
+        lineToBarChart.getStrokeDashArray().addAll(15d, 5d, 15d, 15d, 20d);
+
+        Line lineToPieOne = new Line(450, 340, 325, 440);
+        lineToPieOne.setStroke(Color.BLACK);
+        lineToPieOne.setStrokeWidth(2);
+        lineToPieOne.setStrokeLineCap(StrokeLineCap.BUTT);
+        lineToPieOne.getStrokeDashArray().addAll(15d, 5d, 15d, 15d, 20d);
+
+        Line lineToSkin = new Line(530, 370, 530, 425);
+        lineToSkin.setStroke(Color.BLACK);
+        lineToSkin.setStrokeWidth(2);
+        lineToSkin.setStrokeLineCap(StrokeLineCap.BUTT);
+        lineToSkin.getStrokeDashArray().addAll(15d, 5d, 15d, 15d, 20d);
+
+        Line lineToStars = new Line(612, 310, 800, 425);
+        lineToStars.setStroke(Color.BLACK);
+        lineToStars.setStrokeWidth(2);
+        lineToStars.setStrokeLineCap(StrokeLineCap.BUTT);
+        lineToStars.getStrokeDashArray().addAll(15d, 5d, 15d, 15d, 20d);
+
+        Line lineToPieTwo = new Line(612, 240, 760, 190);
+        lineToPieTwo.setStroke(Color.BLACK);
+        lineToPieTwo.setStrokeWidth(2);
+        lineToPieTwo.setStrokeLineCap(StrokeLineCap.BUTT);
+        lineToPieTwo.getStrokeDashArray().addAll(15d, 5d, 15d, 15d, 20d);
+
+        this.getChildren().addAll(dashboard,backButton, chart, caption, barChart, gameStatusIndicator,
+                starsCollectedStack, skinsPane, profile, lineToBarChart, lineToPieOne, lineToSkin, lineToStars, lineToPieTwo, settingsButton, soundButton );
 
         //Adding labels to panel
         copyRightPanel.getChildren().add(copyRightLabel);
@@ -235,5 +296,9 @@ public class DashboardPane extends Pane {
 
     public void addHandler( GameManager.ButtonListener e) {
         backButton.addEventHandler(MouseEvent.MOUSE_CLICKED, e);
+
+        GameManager.ButtonListener settings = e.clone();
+        settings.setIndex(4);
+        settingsButton.addEventHandler(MouseEvent.MOUSE_CLICKED, settings);
     }
 }
