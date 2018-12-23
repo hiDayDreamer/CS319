553f5c88e0480f94fc1eab875a12530a4c1addea
Endi Merkuri <merkuriendi@gmail.com>
2018-12-24 00:28:11 +0300

    Final push

--- unstable/workplace/aldo/tmpsave/TimerRunnable.java
+++
@@ -1,7 +0,0 @@
-//this is an interface that needs to be implemented by each classs that will use
-// the timer. The idea is that the timer should run on GameTimer and the executing class
-// can simply execute runOnTimer() accordingly to make the changes.
-
-public interface TimerRunnable{
-    public void runOnTimer();
-} 
